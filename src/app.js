847bf15a245944848ca5a0fce24c58fa87552130
Rodrigo <rodrigo.noschang1@gmail.com>
2023-08-11 15:37:16 -0300

    Tirando dados de exemplo

--- src/app.js
+++ src/app.js
@@ -1,19 +1,8 @@
-const restaurantes = [
-    {
-        nome: "Fornetto",
-        descricao: "Restaurante Pizzaria"
-    },
-    {
-        nome: "La Tosca",
-        descricao: "Restaurante de comida italiana"
-    }
-]
-
 import express, { json } from 'express';
 
 import { customerRoutes } from './routes/customers/index.js';
 
 export const app = express();
-app.use(express.json());
+app.use(json());
 
 app.use(customerRoutes);
