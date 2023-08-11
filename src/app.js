0db6da23a5afe34d591a8d7de34437d6062314c6
Rodrigo <rodrigo.noschang1@gmail.com>
2023-08-11 15:35:04 -0300

    Adicionando o use express json para receber dados nas requisicoes

--- src/app.js
+++ src/app.js
@@ -1,7 +1,19 @@
-import express from 'express';
+const restaurantes = [
+    {
+        nome: "Fornetto",
+        descricao: "Restaurante Pizzaria"
+    },
+    {
+        nome: "La Tosca",
+        descricao: "Restaurante de comida italiana"
+    }
+]
+
+import express, { json } from 'express';
 
 import { customerRoutes } from './routes/customers/index.js';
 
 export const app = express();
+app.use(express.json());
 
 app.use(customerRoutes);
