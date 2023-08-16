12d4802485b9e9b13547277ca57081f6775636d1
Rodrigo <rodrigo.noschang1@gmail.com>
2023-08-16 17:50:11 -0300

    Implementando tipagem do token

---
+++ src/@types/fastify-jwt.d.ts
@@ -0,0 +1,12 @@
+import '@fastify/jwt';
+
+declare module "@fastify/jwt" {
+    interface FastifyJWT {
+        payload: {
+            type: 'RESTAURANT' | 'CLIENT'
+        },
+        user: {
+            sub: string
+        }
+    }
+}
